45cf80f60a74abc5ad7fff3930b670b0f181d56c
iAmALittleKid <jose.nogueira.working@gmail.com>
2019-07-26 15:21:47 +0100

    Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,9 +33,11 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            else
-                base.CalculateStatistics();
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
         }
     }
 }
